1f396df71ca537f5809b06512864b8987446fffe
calentito <joserafaelponferrada@ceitartessos.com>
2019-11-04 00:27:27 +0100

    <ejercicio del puente>

---
+++ unity/Assets/SCIPTS/azul.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class azul : MonoBehaviour {
+
+	public float velocidad = 100F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.C))
+			transform.Translate (Vector2.up * velocidad * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (Vector2.down * velocidad * Time.deltaTime);
+		
+	}
+}
---
+++ unity/Assets/SCIPTS/rojo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rojo : MonoBehaviour {
+
+	public float velocidad = 100F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (Vector2.left * velocidad * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.B))
+			transform.Translate (Vector2.right * velocidad * Time.deltaTime);
+		
+	}
+}
